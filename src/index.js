b403972e3f0d7f6449f859287eda26c64bb0e506
raresmigea <raresini@yahoo.com>
2020-04-27 23:31:47 +0300

    added console.log function on click

--- src/index.js
+++ src/index.js
@@ -3,35 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
+  func(x) {
+    console.log(x);
   }
   render() {
     return (
-      <button className='square' onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button
+        className='square'
+        onClick={() => this.func(this.func(this.props.value))}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={i} />;
   }
 
   render() {
     const status = 'Next player: X';
-
     return (
       <div>
         <div className='status'>{status}</div>

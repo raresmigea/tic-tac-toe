28f28843401f934fcbce952b6d7c75cafd4e9a3b
raresmigea <raresini@yahoo.com>
2020-04-28 00:15:14 +0300

    added X and O alternate

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className='square' onClick={() => props.onClick({ value: 'X' })}>
+    <button className='square' onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -19,8 +19,8 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {

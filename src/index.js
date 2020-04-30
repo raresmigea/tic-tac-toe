498e08c98c9efe1baea3f65017319663c02ea6de
raresmigea <raresini@yahoo.com>
2020-05-01 00:58:14 +0300

    created components folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './Game';
+import Game from './components/Game';
 
 ReactDOM.render(<Game />, document.getElementById('root'));

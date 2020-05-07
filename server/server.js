840605a6a8db006488f35f958c39ec6fdb956356
raresmigea <raresini@yahoo.com>
2020-05-07 21:15:51 +0300

    added route on back-end

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.get('/here', function (req, res) {
+  res.send('hello from the back-end.');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`server is listening to ${port}`);
